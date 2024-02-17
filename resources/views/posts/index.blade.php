6cd7c581a117ca6997022e2bee5437e956110aea
Nibir Ahmed <nibirahmed9611@gmail.com>
2024-02-17 18:49:39 +0600

    potsts model added

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Posts</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container mt-5 pt-5">
+        <h1>Posts</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Content</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($posts as $post)
+                    <tr>
+                        <td>{{ $post->id }}</td>
+                        <td>{{ $post->title }}</td>
+                        <td>{{ $post->content }}</td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="3">No posts found.</td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+        <div class="mt-5">
+            {{ $posts->links() }}
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
