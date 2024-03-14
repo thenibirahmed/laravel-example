2f918fd41772fbdb51fe6ff6ab8cd2396ee48baf
Nibir Ahmed <nibirahmed9611@gmail.com>
2024-03-14 15:59:32 +0600

    relationship added

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['post_id', 'comment'];
+
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
