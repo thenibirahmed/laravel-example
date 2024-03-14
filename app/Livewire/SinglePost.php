4b2c19a38b7bbd644409fa4b64c446efc8370351
Nibir Ahmed <nibirahmed9611@gmail.com>
2024-03-14 16:08:38 +0600

    comment working

--- app/Livewire/SinglePost.php
+++ app/Livewire/SinglePost.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Comment;
 use App\Models\Post;
 use Livewire\Component;
 
@@ -9,9 +10,23 @@ class SinglePost extends Component
 {
     public $post;
 
+    public $comment;
+
     public function mount($id)
     {
         $this->post = Post::find($id);
+    }
+
+    public function addComment()
+    {
+        Comment::create([
+            'comment' => $this->comment,
+            'post_id' => $this->post->id,
+        ]);
+
+        $this->comment = '';
+
+        $this->mount($this->post->id);
     }
 
     public function render()
--- resources/views/livewire/single-post.blade.php
+++ resources/views/livewire/single-post.blade.php
@@ -3,5 +3,32 @@
         <div class="col-md-6">
             <h1>{{ $post->title }}</h1>
         </div>
+
+        <div class="d-flex">
+            <input type="text" class="form-control" wire:model='comment'>
+            <button class="btn btn-primary mt-2" wire:click='addComment'>Add Comment</button>
+        </div>
+
+        <h3>Comments</h3>
+        <table class="table mt-4">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Comment</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($post->comments as $comment)
+                    <tr>
+                        <td>{{ $comment->id }}</td>
+                        <td>{{ $comment->comment }}</td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="2">No posts found.</td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
     </div>
 </div>
